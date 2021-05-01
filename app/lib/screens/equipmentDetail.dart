97b44a93618806007bc283e2223a92d31906a160
Anant Verma <vermaanant25@gmail.com>
2021-05-01 19:49:50 +0530

    create equipment detail screen

---
+++ app/lib/screens/equipmentDetail.dart
@@ -0,0 +1,32 @@
+import 'package:app_rec_task/models/equipment.dart';
+import 'package:flutter/material.dart';
+
+class EquipmentDetail extends StatelessWidget {
+  const EquipmentDetail({Key key, this.equipment}) : super(key: key);
+
+  final Equipment equipment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Test')),
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                child: Text(equipment.title),
+              ),
+              Container(
+                child: Text(equipment.desc),
+              ),
+              Container(
+                child: Text(equipment.imageUrl),
+              ),
+              Container(
+                child: Text(equipment.date),
+              ),
+            ],
+          ),
+        ));
+  }
+}
