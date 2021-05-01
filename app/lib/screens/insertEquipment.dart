38eeaee12f2dc095f02f16e54e44c4f6bec50e70
Anant Verma <vermaanant25@gmail.com>
2021-05-01 19:39:46 +0530

    create insert equipment screen

---
+++ app/lib/screens/insertEquipment.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InsertEquipment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Insert Equipment")),
+      body: Container(
+        child: Center(child: Text("Insert")),
+      ),
+    );
+  }
+}
