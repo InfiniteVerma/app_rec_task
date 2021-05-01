b6a364ccbc73716327ba7c9b066a193650198fcd
Anant Verma <vermaanant25@gmail.com>
2021-05-01 19:39:32 +0530

    refactor code and add button to insert page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_rec_task/screens/insertEquipment.dart';
 import 'package:app_rec_task/screens/listEquipment.dart';
 import 'package:flutter/material.dart';
 
@@ -13,11 +14,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Test'),
-          ),
-          body: EquipmentList()),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Test'),
+      ),
+      body: EquipmentList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+          print("Insert button pressed");
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => InsertEquipment()));
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
     );
   }
 }
