464ed73af20afd8954e7c70f21ee849a1b72a59e
Anant Verma <vermaanant25@gmail.com>
2021-05-02 07:31:07 +0530

    get data from api in listview

--- app/lib/screens/listEquipment.dart
+++ app/lib/screens/listEquipment.dart
@@ -1,3 +1,4 @@
+import 'package:app_rec_task/http_service.dart';
 import 'package:app_rec_task/models/equipment.dart';
 import 'package:app_rec_task/screens/equipmentDetail.dart';
 import 'package:flutter/material.dart';
@@ -8,54 +9,39 @@ class EquipmentList extends StatefulWidget {
 }
 
 class _EquipmentListState extends State<EquipmentList> {
-  List<Equipment> _list = List.empty(growable: true);
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _list.add(Equipment(
-        id: 1,
-        title: "TItle1",
-        desc: "Desc1",
-        imageUrl: "www.google.com",
-        date: "today"));
-    _list.add(Equipment(
-        id: 2,
-        title: "TItle2",
-        desc: "Desc2",
-        imageUrl: "www.google.com",
-        date: "today"));
-    _list.add(Equipment(
-        id: 3,
-        title: "TItle3",
-        desc: "Desc3",
-        imageUrl: "www.google.com",
-        date: "today"));
-    _list.add(Equipment(
-        id: 4,
-        title: "TItle4",
-        desc: "Desc4",
-        imageUrl: "www.google.com",
-        date: "today"));
-  }
+  final HttpService httpService = HttpService();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ListView.builder(
-          itemCount: _list.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                print("AT item: $index");
-                Navigator.push(context, MaterialPageRoute(builder: (context) => EquipmentDetail(equipment: _list[index])));
-              },
-              child: ListTile(
-                title: Text(_list[index].title),
-              ),
-            );
-          }),
+      child: FutureBuilder(
+        future: httpService.getEquipments(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Equipment>> snapshot) {
+          if (snapshot.hasData) {
+            List<Equipment> equipments = snapshot.data;
+            return ListView.builder(
+                itemCount: equipments.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => EquipmentDetail(
+                              equipment: equipments[index],
+                            ))),
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        equipments[index].title,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  );
+                });
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
