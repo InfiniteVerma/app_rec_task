746ec26cb0400d5581df32f54a75cc4cc433b1c4
Anant Verma <vermaanant25@gmail.com>
2021-05-02 07:30:45 +0530

    edit Equipment model

--- app/lib/models/equipment.dart
+++ app/lib/models/equipment.dart
@@ -2,8 +2,20 @@ class Equipment {
   int id;
   String title;
   String desc;
-  String imageUrl;
+  String location;
   String date;
+  int phoneNumber;
 
-  Equipment({this.id, this.title, this.desc, this.imageUrl, this.date});
+  Equipment({this.id, this.title, this.desc, this.location, this.date, this.phoneNumber});
+
+  factory Equipment.fromJson(Map<String, dynamic> json){
+    return Equipment(
+      id : json['id'] as int,
+      title: json['title'] as String,
+      desc: json['desc'] as String,
+      location: json['location'] as String,
+      date: json['date'] as String,
+      phoneNumber: json['phoneNumber'] as int
+    );
+  }
 }
