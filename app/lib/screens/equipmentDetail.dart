4708b259a965424b9670d565e63ae8ae7e4ef3e2
Anant Verma <vermaanant25@gmail.com>
2021-05-02 09:29:44 +0530

    UI fixes

--- app/lib/screens/equipmentDetail.dart
+++ app/lib/screens/equipmentDetail.dart
@@ -8,25 +8,59 @@ class EquipmentDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var date = DateTime.parse(equipment.date);
+    var mydate = date.year.toString() +
+        "-" +
+        date.month.toString() +
+        "-" +
+        date.day.toString();
+
     return Scaffold(
         appBar: AppBar(title: Text('Test')),
         body: Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           child: Column(
             children: [
               Container(
-                child: Text(equipment.title),
+                padding: const EdgeInsets.all(5),
+                height: 200,
+                decoration: BoxDecoration(border: Border.all()),
+                child: Center(
+                  child: Text('Image'),
+                ),
               ),
-              Container(
-                child: Text(equipment.desc),
-              ),
-              Container(
-                child: Text(equipment.location),
-              ),
-              Container(
-                child: Text(equipment.date),
-              ),
+              buildTextContainer("Title", equipment.title),
+              buildTextContainer("Desc", equipment.desc),
+              buildTextContainer("Location", equipment.location),
+              buildTextContainer("Phone", equipment.phoneNumber.toString()),
+              buildTextContainer("Date", mydate),
             ],
           ),
         ));
+  }
+
+  Container buildTextContainer(String key, String text) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              key,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
